32cf1a4604f0c9158adabc91f431683fff272187
Dmitry Lihachev <lda@openteam.ru>
2011-04-02 17:03:03 +0700

    i18n date

--- lib/formtastic_date_as_string/formtastic.rb
+++ lib/formtastic_date_as_string/formtastic.rb
@@ -3,6 +3,7 @@ Formtastic::SemanticFormBuilder.class_eval do
   def date_input(method, options = {})
     options[:input_html] ||= {}
     options[:input_html][:class] = [options[:input_html][:class], :date].compact.join ' '
+    options[:input_html][:value] = I18n.l(object[method]) if object[method].is_a? Date
     options[:wrapper_html] ||= {}
     options[:input_html][:class] = [options[:wrapper_html][:class], :date].compact.join ' '
     string_input method, options
