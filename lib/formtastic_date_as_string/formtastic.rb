bfc688f0f3a0edbcb919053aafef3ced8b79db80
Evgeny Lapin <lev@openteam.ru>
2011-07-14 14:10:50 +0700

    support mongoid

--- lib/formtastic_date_as_string/formtastic.rb
+++ lib/formtastic_date_as_string/formtastic.rb
@@ -9,9 +9,20 @@ Formtastic::SemanticFormBuilder.class_eval do
     string_input method, options
   end
 
+  def datetime_input(method, options = {})
+    options[:input_html] ||= {}
+    options[:input_html][:class] = [options[:input_html][:class], :datetime].compact.join ' '
+    options[:input_html][:value] = I18n.l(object[method]) if object[method].is_a? DateTime
+    options[:wrapper_html] ||= {}
+    options[:input_html][:class] = [options[:wrapper_html][:class], :datetime].compact.join ' '
+    string_input method, options
+  end
+
   def default_input_type_with_date(method, options={})
-    if date_column? method
+    if column_of_type? method, :date
       :date
+    elsif column_of_type? method, :datetime
+      :datetime
     else
       default_input_type_without_date method, options
     end
@@ -21,8 +32,21 @@ Formtastic::SemanticFormBuilder.class_eval do
 
   protected
 
-    def date_column?(method)
-      object.respond_to?(:column_for_attribute) &&
-         object.column_for_attribute(method).try(:type) == :date
+    def column_of_type?(method, type)
+      (mongoid_type(method) || active_record_type(method)).to_s.downcase == type.to_s
     end
+
+    def active_record_type(method)
+      if defined?(ActiveRecord::Base) && object.is_a?(ActiveRecord::Base) && object.column_for_attribute(method)
+        return object.column_for_attribute(method).try(:type)
+      end
+    end
+
+    def mongoid_type(method)
+      if defined?(Mongoid::Document) && object.is_a?(Mongoid::Document) && object.fields[method.to_s]
+        return object.fields[method.to_s].options[:type]
+      end
+    end
+
 end
+
